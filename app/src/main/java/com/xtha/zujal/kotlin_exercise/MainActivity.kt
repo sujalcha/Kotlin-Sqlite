c350244635e254788ebaf83ea780ee68b5c1cc5e
sujalcha <zujalxtha@gmail.com>
2019-10-04 13:57:53 +1000

    Adding model class product and instantiating objects

--- app/src/main/java/com/xtha/zujal/kotlin_exercise/MainActivity.kt
+++ app/src/main/java/com/xtha/zujal/kotlin_exercise/MainActivity.kt
@@ -2,11 +2,23 @@ package com.xtha.zujal.kotlin_exercise
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.xtha.zujal.kotlin_exercise.Model.Product
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Printing your first Hello Worrld Text
+        println( "This is a Hello Word Text")
+
+        // Instantiating your first object from class product
+        var product = Product()
+        product.productName = "Chair"
+
+        println(product.productName)
+        println("This is " + product.productName + " which is a product name")
+        println("This is ${product.productName} which is a product name")
     }
 }
---
+++ app/src/main/java/com/xtha/zujal/kotlin_exercise/Model/Product.kt
@@ -0,0 +1,11 @@
+package com.xtha.zujal.kotlin_exercise.Model
+
+class  Product{
+
+    var productId : Int = 0
+    var productName : String = ""
+    var productAvailability : Boolean = false
+    var productPrice : Double = 5.55
+    var productQuantity : Int = 0
+
+}
